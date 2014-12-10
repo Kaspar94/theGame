a45d451ca5e28941cf1973feb6830c7d19fde826
kaspar94 <kaspar94@tdl.ee>
2014-12-11 00:08:36 +0200

    jh

--- game/main.py
+++ game/main.py
@@ -217,12 +217,9 @@ class Game:
                 self.del_bloks()
                 self.del_enemies()
                 boss = Enemy(self.enemytype["boss"])
-                boss.rect.w,boss.rect.h = (self.level*100,self.level*100)
-                #boss.type["h"] = (self.level*200)
-                #boss.type["w"] = (self.level*200)
-                boss.rect.h = (self.level*200)
-                boss.rect.w = (self.level*200)
-                boss.elusi = (self.level*5)
+                boss.rect.h = (self.level*100)
+                boss.rect.w = (self.level*100)
+                boss.elusi = (self.level*3)
                 self.pahad.append(boss)
                 self.bossInit = True
             else:
@@ -276,15 +273,12 @@ class Game:
                 if(collision(bullet.rect, enemy.rect)):
                     if (enemy.getRekt(bullet.dmg)):
                         if("boss" in enemy.type):
-                            try:
+                            try:  # viskab mingi errori non-integer stop for randrange(). Ei oska muudmoodi lahendada :(
                                 miniBoss = Enemy(enemy.type.copy(),True,enemy.rect.x+random.randint(0,enemy.rect.w),enemy.rect.y)
                                 miniBoss2 = Enemy(enemy.type.copy(),True,enemy.rect.x+random.randint(0,enemy.rect.w),enemy.rect.y+enemy.rect.h)
-                                miniBoss.rect.w = enemy.rect.w/2
-                                miniBoss.rect.h = enemy.rect.h/2
-                                miniBoss2.rect.w = enemy.rect.w/2
-                                miniBoss2.rect.h = enemy.rect.h/2
-                                miniBoss.elusi = (self.level*5)
-                                miniBoss2.elusi = (self.level*5)
+                                miniBoss.rect.w,miniBoss.rect.h = enemy.rect.w/2,enemy.rect.h/2
+                                miniBoss2.rect.w,miniBoss2.rect.h = enemy.rect.w/2,enemy.rect.h/2
+                                miniBoss.elusi,miniBoss2.elusi = (self.level*5),(self.level*5)
                                 if(miniBoss.rect.h > 10): # kontrollime et liiga mini poleks
                                     self.pahad.append(miniBoss)
                                     self.pahad.append(miniBoss2)
