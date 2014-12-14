4a9c734a0400306b73bea84b24ad9dddc37ea7ce
kaspar94 <kaspar94@tdl.ee>
2014-12-14 16:48:18 +0200

    laser

--- game/laser.py
+++ game/laser.py
@@ -1,3 +1,14 @@
 from object_functions import *
+from timer import Timer
+import random
 
+class Laser():
+    def __init__(self,speed):
+        self.delay = Timer(speed)
+        self.wait = Timer(1)
+        self.type = random
 
+    def update(self):
+        pass
+    def show(self):
+        pass
